eaa4b660d528b4da8acfa777d016326d98d2405b
Ekaterina Ivanova <ekaterina.ivanova@saltpepper.ru>
2016-05-27 11:43:57 +0300

    Remove useless parts (views, models), add redux, write redux counter and tests

--- app/scripts/__test__/test.js
+++ app/scripts/__test__/test.js
@@ -1,10 +1,35 @@
 /* eslint no-unused-expressions: 0 */
-describe('example test', () => {
-  it('dont breaks math', () => {
-    expect(2).to.be.eql(4 / 2);
-  });
 
-  it('dont breaks logic', () => {
-    expect(1).to.be.ok;
+import App from '../app';
+
+describe('CounterModel test', () => {
+  let app;
+  beforeEach(() => {
+    app = new App();
+  });
+  it('creates store with initial state', () => {
+    expect(app.store.getState()).to.be.eql(0);
+  });
+  it('increments 0 to 1', () => {
+    app.store.dispatch({type: 'INCREMENT'});
+    expect(app.store.getState()).to.be.eql(1);
+  });
+  it('increments 1 to 2', () => {
+    app.store.dispatch({type: 'INCREMENT'});
+    app.store.dispatch({type: 'INCREMENT'});
+    expect(app.store.getState()).to.be.eql(2);
+  });
+  it('decrements 1 to 0', () => {
+    app.store.dispatch({type: 'INCREMENT'});
+    app.store.dispatch({type: 'DECREMENT'});
+    expect(app.store.getState()).to.be.eql(0);
+  });
+  it('decrements 0 to -1', () => {
+    app.store.dispatch({type: 'DECREMENT'});
+    expect(app.store.getState()).to.be.eql(-1);
+  });
+  it('dont change state on unknown action', () => {
+    app.store.dispatch({type: 'UNKNOWN'});
+    expect(app.store.getState()).to.be.eql(0);
   });
 });

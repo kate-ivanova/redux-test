ac4cc65e1c7e43906fac3df4d61695c4bd8f827e
Ekaterina Ivanova <ekaterina.ivanova@saltpepper.ru>
2016-06-03 17:49:53 +0300

    Add react-router

--- app/scripts/components/ContentLayout.js
+++ app/scripts/components/ContentLayout.js
@@ -2,11 +2,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
+import {Router, Route, browserHistory} from 'react-router';
 import TodoWidget from 'components/TodoWidget';
 
 const ContentLayout = ({store}) => (
   <Provider store={store}>
-    <TodoWidget />
+    <Router history={browserHistory}>
+      <Route path="/" component={TodoWidget} />
+    </Router>
   </Provider>,
 );
 
